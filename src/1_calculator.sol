dd7736e6de2bbcd2fb983dd53ac144d0935015f0
Joe Marron <marronjo@tcd.ie>
2025-01-28 15:09:15 +0900

    add remaining calculator operations

--- src/1_calculator.sol
+++ src/1_calculator.sol
@@ -3,15 +3,39 @@ pragma solidity ^0.8.19;
 
 import { FHE, inEuint8, euint8 } from "@fhenixprotocol/contracts/FHE.sol";
 
+ /**
+ * Implement these methods to make the tests pass
+ * Ignore overflow, since FHE add wraps overflowing values
+ * All values are using 8 bit lengths for simplicity
+ *
+ * Hint : you need to change the inEuints into euints before using
+ */
 contract Calculator {
     /**
-     * Implement this method to make the tests pass
-     * Add the euints a and b together and return the result
-     * Ignore overflow, since FHE add wraps overflowing values
-     *
-     * Hint : you need to change the inEuints into euints before adding
+     * Add the two inputs and return the value as an 8 bit euint (a + b)
      */
     function add(inEuint8 calldata a, inEuint8 calldata b) public pure returns (euint8) {
+        // write your code here
+    }
+
+    /**
+     * Subtract b from a and return the value as an 8 bit euint (a - b)
+     */
+    function sub(inEuint8 calldata a, inEuint8 calldata b) public pure returns (euint8) {
+        // write your code here
+    }
+
+    /**
+     * Multiply the two inputs and return the value as an 8 bit euint (a * b)
+     */
+    function mul(inEuint8 calldata a, inEuint8 calldata b) public pure returns (euint8) {
+        // write your code here
+    }
+
+    /**
+     * Divide input a by input b and return the value as an 8 bit euint (a / b)
+     */
+    function div(inEuint8 calldata a, inEuint8 calldata b) public pure returns (euint8) {
         // write your code here
     }
 }
